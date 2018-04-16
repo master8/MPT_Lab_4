7f431621e739bb1e39e55fbabf25a5cf3ba54a6c
master8 <master8.michael@gmail.com>
2018-04-16 22:08:57 +0500

    add files

---
+++ Lab_4/Calculator.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.CodeAnalysis.Emit;
+using System.Reflection;
+
+namespace Lab_4
+{
+    class Calculator
+    {
+
+        delegate double ExecMethod(double x, double y);
+
+        private ExecMethod delegat;
+
+        private SyntaxTree tree;
+
+        public Calculator(string exp)
+        {
+            this.tree = CSharpSyntaxTree.ParseText(@"using System;
+
+namespace Lab_4
+{
+    class First
+    {
+        public double exec(double x, double y)
+        {
+            return " + exp + @";
+        }
+    }
+}
+");
+
+            var compilation = CSharpCompilation.Create("first", 
+                new[] { tree }, 
+                new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) },
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            var stream = new MemoryStream();
+            EmitResult result = compilation.Emit(stream);
+
+            if (!result.Success)
+            {
+                foreach (Diagnostic item in result.Diagnostics)
+                {
+                    Console.Error.WriteLine($"{item.Id}: {item.GetMessage()}");
+                }
+                return;
+            }
+
+            stream.Seek(0, SeekOrigin.Begin);
+            Assembly assembly = Assembly.Load(stream.ToArray());
+            Type type = assembly.GetType("Lab_4.First");
+            ConstructorInfo constructorInfo = type.GetConstructors().First();
+            var instance = constructorInfo.Invoke(null);
+            MethodInfo method = type.GetMethod("exec");
+            delegat = (ExecMethod)method.CreateDelegate(typeof(ExecMethod), instance);
+        }
+
+        public double Calc(double x, double y)
+        {
+            return delegat(x, y);
+        }
+    }
+}
---
+++ Lab_4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long start = DateTime.Now.Ticks;
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"{i} Result: " + new Calculator("Math.Pow(x * 2 + Math.PI + Math.Sin(x * 3 * y), 3)")
+                    .Calc(1.5, 0.17));
+            }
+
+            long end1 = DateTime.Now.Ticks - start;
+
+            long start2 = DateTime.Now.Ticks;
+            var calculator = new Calculator("Math.Pow(x * 2 + Math.PI + Math.Sin(x * 3 * y), 3)");
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"{i} Result: " + calculator.Calc(1.5, 0.17));
+            }
+
+            long end2 = DateTime.Now.Ticks - start2;
+
+            Console.WriteLine($"End 1: {end1}");
+            Console.WriteLine($"End 2: {end2}");
+            Console.WriteLine($"Diff: {end1 - end2}");
+        }
+    }
+}
